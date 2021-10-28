bc9b546280902a0b5d7b739a94a7854029001ee5
AkshatBanthiya <akshatbanthiya07@gmail.com>
2021-10-28 22:36:51 +0530

    API

---
+++ database/schema/schema.js
@@ -0,0 +1,19 @@
+//Products
+/*
+id: id
+Name: String
+Price: Number
+Category: Category_id
+*/
+
+//Category
+/*
+id: id
+Name: String
+*/
+
+//order
+/*
+Total-Price: Number
+Product: Array of Products[Product_id]
+*/
--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require("express")
+const database=require('./database/db')
 const app=express()
 
-app.get('/products/cases')
-app.get('/products/protections')
-app.get('/products/bands')
-
-app.get('/products/:categories',(req,res)=>{
-    console.log(req.params)
-    res.send("PING")
-})
+app.use(express.json())
 
 app.listen(3001, ()=>{
     console.log("LISTENING AT PORT 3001")
